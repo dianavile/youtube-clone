610b3a6307a569d417c518307e89fda8cf093e26
dianavile <diana.vile@gmail.com>
2020-12-01 14:33:32 +0100

    add material icons to Header Component

--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallSharpIcon from '@material-ui/icons/VideoCallSharp';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
 
 function Header() {
     return (
         <div className="header">
-            <h1>Header</h1>
+            <MenuIcon />
+            <img
+                className="header__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+                alt="YouTube_Logo"
+            />
+            <input type="text" />
+            <SearchIcon />
+            <VideoCallSharpIcon />
+            <AppsIcon />
+            <NotificationsIcon />
+            <Avatar />
         </div>
     )
 }
